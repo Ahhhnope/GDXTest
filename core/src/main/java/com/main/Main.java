354117086e5ef0f541fb739c25b45ae6198e1ef8
silly <91793489+luvlowdetailmode@users.noreply.github.com>
2025-04-11 13:45:05 +0700

    aw

--- core/src/main/java/com/main/Main.java
+++ core/src/main/java/com/main/Main.java
@@ -6,25 +6,28 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.Text;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
-    private int lmao = 256;
+    Texture playerTexture;
+    SpriteBatch batch;
 
     @Override
     public void create() {
-
+        playerTexture = new Texture("Spaceships/spaceships.png");
+        batch = new SpriteBatch();
     }
 
     @Override
     public void render() {
-
+        batch.begin();
+        batch.draw(playerTexture, 100, 100, 64, 64);
+        batch.end();
     }
 
     @Override
     public void dispose() {
-
+        playerTexture.dispose();
     }
 }
