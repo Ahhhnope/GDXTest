0e3914b5a01c98febcd9c0408e34311beabf32e2
abc13379999 <binhnhth03490@fpt.edu.vn>
2025-04-11 15:10:49 +0700

    ád

--- core/src/main/java/com/main/Main.java
+++ core/src/main/java/com/main/Main.java
@@ -6,28 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import org.w3c.dom.Text;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    Texture playerTexture;
-    SpriteBatch batch;
 
-    @Override
-    public void create() {
-        playerTexture = new Texture("Spaceships/spaceships.png");
-        batch = new SpriteBatch();
-    }
-
-    @Override
-    public void render() {
-        batch.begin();
-        batch.draw(playerTexture, 200, 200, 32, 32);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        playerTexture.dispose();
-    }
 }
