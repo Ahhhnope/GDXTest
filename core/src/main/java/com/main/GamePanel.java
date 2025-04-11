6dd1a4ff07aa63a71075f1eca63dca62cb2af986
Ahhhnope <magelova32@gmail.com>
2025-04-11 13:56:41 +0700

    commit

--- core/src/main/java/com/main/GamePanel.java
+++ core/src/main/java/com/main/GamePanel.java
@@ -1,6 +1,9 @@
 package com.main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
     public int fps = 120;
     public int ups = 200;
 
@@ -8,10 +11,43 @@ public class GamePanel {
 
     public GamePanel() {
         gameThread = new Thread(this);
+        gameThread.start();
     }
 
     @Override
-    public run() {
+    public void run() {
+        double drawInterval = 1000000000 / fps;
+        double updateInterval = 1000000000 / ups;
 
+
+        int fpsCount = 0;
+        int updateCount = 0;
+        long lastUpdate = System.currentTimeMillis();
+
+        double drawTime = 0;
+        double updateTime = 0;
+
+        while (true) {
+            long currTime = System.nanoTime();
+            updateTime = (currTime - lastUpdate) / updateInterval;
+            drawInterval = (currTime - lastUpdate) / drawInterval;
+
+            if (updateTime >= 1) {
+                update();
+            }
+
+            if (drawTime >= 1) {
+                repaint();
+            }
+        }
+
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
