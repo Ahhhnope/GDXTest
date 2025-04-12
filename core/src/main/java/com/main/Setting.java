aff31812ef17f83a16d0108e308a766195bc5d6c
abc13379999 <binhnhth03490@fpt.edu.vn>
2025-04-12 22:33:28 +0700

    ád

--- core/src/main/java/com/main/Setting.java
+++ core/src/main/java/com/main/Setting.java
@@ -1,10 +1,16 @@
 package com.main;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Setting {
 
+    private Texture BackgroundScreen;
+
     public Setting(){
+        //load ảnh background
+        BackgroundScreen = new Texture("Stuffs/backgroundScreen.jpeg");
 
     }
 
@@ -13,9 +19,11 @@ public class Setting {
     }
 
     public void render(SpriteBatch batch){
-
+        //nền Background
+        batch.draw(BackgroundScreen, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     public void Dispose(){
+        BackgroundScreen.dispose();
     }
 }
