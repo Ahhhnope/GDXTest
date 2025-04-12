d7f7b5051e645874a3e07c439aec1e5642c86af5
Ahhhnope <magelova32@gmail.com>
2025-04-12 14:21:32 +0700

    E

--- core/src/main/java/com/main/GameManager.java
+++ core/src/main/java/com/main/GameManager.java
@@ -1,14 +1,9 @@
 package com.main;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
+
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import javax.swing.*;
-import java.awt.*;
+
 
 public class GameManager {
     private SpriteBatch batch;
