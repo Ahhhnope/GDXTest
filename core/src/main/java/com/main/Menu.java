6631b695aa44df6c9dc658a0eac2460a87cd795a
silly <91793489+luvlowdetailmode@users.noreply.github.com>
2025-04-15 18:10:57 +0700

    UI Soundeffects

--- core/src/main/java/com/main/Menu.java
+++ core/src/main/java/com/main/Menu.java
@@ -65,23 +65,23 @@ public class Menu {
 //            Đảo ngược y (Vì một lí do nào đó "Gdx.input.getY()" trả về tọa độ y tính từ trên xuống =w=)
 
             if (btnPlayHitbox.contains(x, y)) {
-                SoundManager.load();
+                SoundManager.play("click");
                 GameManager.currScreen = "game";
             }
 
             if (btnSettingHitbox.contains(x, y)) {
-                SoundManager.load();
+                SoundManager.play("click");
                 GameManager.currScreen = "setting";
             }
 
             if (btnScoreHitbox.contains(x, y)) {
-                SoundManager.load();
+                SoundManager.play("click");
                 GameManager.currScreen = "scoreboard";
 
             }
 
             if (btnQuitHitbox.contains(x, y)) {
-                SoundManager.load();
+                SoundManager.play("click");
                 GameManager.currScreen = "quit";
 
 
